a82a06a225aebe2ede9c63d391f1f6fed22d1563
emmytech07 <ilesanmiemmanuel07@gmail.com>
2022-04-29 11:50:47 +0100

    Adding A django backend registration form

---
+++ myapp/models.py
@@ -0,0 +1,9 @@
+from pyexpat import model
+from unicodedata import name
+from django.db import models
+
+# Create your models here.
+
+class Feature(models.Model):
+    name = models.CharField(max_length=300)
+    details = models.CharField(max_length=400)
